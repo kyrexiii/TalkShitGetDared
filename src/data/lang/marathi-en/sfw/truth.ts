73e6d1e2b68dab27965350f137c0f2d4f6c3c4ed
GodKode69 <knownasraghav@gmail.com>
2026-05-26 15:06:06 +0530

    testing automatic language loader with marathi-en sfw truth

---
+++ src/data/lang/marathi-en/sfw/truth.ts
@@ -0,0 +1,11 @@
+import { Prompt } from '../../../../lib/types/index';
+
+export const truthPrompts: Prompt[] = [
+  {
+    id: 'mn_sfw_t001',
+    text: 'Kya tumne kabhi bimari ka bahana banake chutti li hai?',
+    contributor: 'kyrexiii',
+    difficulty: 'easy',
+    category: 'confession',
+  },
+];
